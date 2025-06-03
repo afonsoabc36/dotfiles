3a8e85dc9728c90195f28ce2c467adc8c12c8e78
afonsoabc36 <silva.af36@gmail.com>
2025-06-03 01:48:15 +0100

    feat: battery life notifications

---
+++ i3/.config/i3/scripts/battery_notify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BATTERY_PERCENTAGE=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | rg -o 'percentage:\s+(\d+)%' | rg -o '\d+'`
+
+if [ $BATTERY_PERCENTAGE -le 13 ]; then
+    notify-send --urgency=critical 'LOW BATTERY' 'Go get your charger now!'
+elif [ $BATTERY_PERCENTAGE -le 25 ]; then
+    notify-send --urgency=normal --expire-time=10000 'WARNING' 'Low battery'
+fi
