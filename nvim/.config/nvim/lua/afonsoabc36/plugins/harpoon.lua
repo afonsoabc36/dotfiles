a367481f55b28153661103d767f66a22b491a8d4
Afonso Silva <220229694+cx-afonso-silva@users.noreply.github.com>
2025-09-15 08:39:08 +0100

    chore(nvim): upgrading harpoon to harpoon2

--- nvim/.config/nvim/lua/afonsoabc36/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/afonsoabc36/plugins/harpoon.lua
@@ -1,50 +1,39 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
+
   config = function()
     local harpoon = require("harpoon")
-    local harpoonMarks = require("harpoon.mark")
-    local harpoonUI = require("harpoon.ui")
-    harpoon.setup({
-      global_settings = {
-        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-        save_on_toggle = false,
-        -- saves the harpoon file upon every change. disabling is unrecommended.
-        save_on_change = true,
-        -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-        enter_on_sendcmd = false,
-        -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-        tmux_autoclose_windows = false,
-        -- filetypes that you want to prevent from adding to the harpoon list menu.
-        excluded_filetypes = { "harpoon" },
-        -- set marks specific to each git branch inside git repository
-        mark_branch = false,
-        -- enable tabline with harpoon marks
-        tabline = false,
-        tabline_prefix = "   ",
-        tabline_suffix = "   ",
-      },
-    })
-
-    local opts = { noremap = true, silent = true }
+    local harpoon_extensions = require("harpoon.extensions")
+    harpoon:setup()
 
     vim.keymap.set("n", "<leader>a", function()
-      harpoonMarks.add_file()
-    end, opts)
+      harpoon:list():add()
+    end)
     vim.keymap.set("n", "<leader>h", function()
-      harpoonUI.toggle_quick_menu()
-    end, opts)
-    vim.keymap.set("n", "<M-1>", function()
-      harpoonUI.nav_file(1)
-    end, opts)
-    vim.keymap.set("n", "<M-2>", function()
-      harpoonUI.nav_file(2)
-    end, opts)
-    vim.keymap.set("n", "<M-3>", function()
-      harpoonUI.nav_file(3)
-    end, opts)
-    vim.keymap.set("n", "<M-4>", function()
-      harpoonUI.nav_file(4)
-    end, opts)
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+    vim.keymap.set("n", "<C-1>", function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set("n", "<C-2>", function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set("n", "<C-3>", function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set("n", "<C-4>", function()
+      harpoon:list():select(4)
+    end)
+    vim.keymap.set("n", "<C-S-P>", function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set("n", "<C-S-N>", function()
+      harpoon:list():next()
+    end)
+
+    -- Highlights
+    harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
   end,
 }
