3278a551548f4b7f35eeeba9e9af24a82fdbccbf
Afonso Silva <220229694+cx-afonso-silva@users.noreply.github.com>
2025-12-10 11:43:09 +0000

    chore(nvim): minor plugin changes

--- nvim/.config/nvim/lua/afonsoabc36/plugins/codesnap.lua
+++ nvim/.config/nvim/lua/afonsoabc36/plugins/codesnap.lua
@@ -2,17 +2,22 @@ return {
   "mistricky/codesnap.nvim",
   config = function()
     local codesnap = require("codesnap")
-
     codesnap.setup({
       border = "rounded",
       has_breadcrumbs = true,
-      bg_theme = "grape",
+      bg_theme = "sea",
       watermark = "",
+      code_font_family = "FiraCode Nerd Font",
       save_path = "~/Pictures",
+      has_line_number = true,
     })
 
     local keymap = vim.keymap
-    keymap.set("n", "<leader>cc", "<cmd>CodeSnap<CR>", { desc = "Save selected code snapshot into clipboard" })
-    keymap.set("n", "<leader>cs", "<cmd>CodeSnapSave<CR>", { desc = "Save selected code snapshot in ~/Pictures" })
+    keymap.set("n", "<leader>cs", "<cmd>CodeSnap<CR>", { desc = "Save selected code snapshot into clipboard" })
+    keymap.set("x", "<leader>cs", "<cmd>'<,'>CodeSnap<CR>", { desc = "Save selected code snapshot into clipboard" })
+    keymap.set("n", "<leader>cc", "<cmd>CodeSnapSave<CR>", { desc = "Save selected code snapshot in ~/Pictures" })
+    keymap.set("x", "<leader>cc", "<cmd>'<,'>CodeSnapSave<CR>", { desc = "Save selected code snapshot in ~/Pictures" })
+    keymap.set("n", "<leader>ch", "<cmd>CodeSnapHighlight<CR>", { desc = "Highlight code inside the editor" })
+    keymap.set("x", "<leader>ch", "<cmd>'<,'>CodeSnapHighlight<CR>", { desc = "Highlight code inside the editor" })
   end,
 }
